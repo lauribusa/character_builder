50a8d09438b48ff491a2fe91d7ac852e140ec10c
davy <dewaegeneer.davy@gmail.com>
2018-02-21 16:53:29 +0100

    component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { LoginComponent } from './login/login.component';
 
-
+const routes = [
+  {path: 'login', component: LoginComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharacterListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/character/characterModel.ts
@@ -0,0 +1,93 @@
+export class characterStats {
+
+    name: String;
+    class: String;
+    level: Number;
+    id: Number;
+    xp: Number;
+    gold: String;
+    maxHP: Number;
+    curHP: Number;
+    maxSP: Number;
+    curSP: Number;
+    str: Number;
+    int: Number;
+    con: Number;
+    agl: Number;
+
+    equip: {
+        rightHand: {
+            name: String;
+            img: String;
+            atk: Number;
+            def: Number;
+            matk: Number;
+            mdef: Number;
+            spd: Number;
+            hit: Number;
+        };
+        leftHand: {
+            name: String;
+            img: String;
+            atk: Number;
+            def: Number;
+            matk: Number;
+            mdef: Number;
+            spd: Number;
+            hit: Number;
+        };
+        headArmor: {
+            name: String;
+            img: String;
+            atk: Number;
+            def: Number;
+            matk: Number;
+            mdef: Number;
+            spd: Number;
+            hit: Number;
+        };
+        bodyArmor: {
+            name: String;
+            img: String;
+            atk: Number;
+            def: Number;
+            matk: Number;
+            mdef: Number;
+            spd: Number;
+            hit: Number;
+        };
+        legArmor: {
+            name: String;
+            img: String;
+            atk: Number;
+            def: Number;
+            matk: Number;
+            mdef: Number;
+            spd: Number;
+            hit: Number;
+        };
+    };
+
+    skills: [
+        [0], {
+            name: String;
+            desc: String;
+        },
+        [1], {
+            name: String;
+            desc: String;
+        },
+        [2], {
+            name: String;
+            desc: String;
+        },
+        [3], {
+            name: String;
+            desc: String;
+        },
+        [4], {
+            name: String;
+            desc: String;
+        }
+    ];
+}
