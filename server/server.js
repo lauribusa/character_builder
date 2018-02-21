0d4f8ebad8a52fa75ca13c3f25ac35c171923baf
Lightbolt <lauri.busa@gmail.com>
2018-02-21 09:21:13 +0100

    updated server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,30 @@
+const Glue = require('glue');
+const routes = require('hapi-routes-plugin');
+const models = require('hapi-moongoose-models-plugin');
+const manifest = {
+	server: {
+		port: 8000,
+		routes: {
+			cors: {
+				origin: ['*']
+			} 
+		} 
+	},
+	register: {
+		plugins: [{ plugin: models, options: { database: 'armory' } }, routes]
+	}
+};
 
+const start = async () => {
+    try {
+      const server = await Glue.compose(manifest);
+      await server.start();
+      console.log('Server started at :' + server.info.uri);
+    } catch (err) {
+      console.log('There was an error');
+      console.error(err);
+      process.exit(1);
+    }
+  };
 
+  start();
